b15b7a9f1b3fdf88f5d6db0414fd41291557c8b8
Christopher McNeal <camcneal@crimson.ua.edu>
2016-11-23 16:29:12 -0600

    resubmit

--- main.js
+++ main.js
@@ -1,11 +1,57 @@
 function makeListItem() {
     var text = $('.text').val();
-
+$.ajax({
+	url: 'https://tiny-za-server.herokuapp.com/collections/Chris_list',
+	type: 'POST',
+	success: function(todo, status, xhr) {
+renderTodo();
+    // navigate to post list
+console.log(todo);
+	},
+	error: function() {
+		// log an error
+		console.log('the item didn\'t work');
+	},
+	contentType: 'application/json',
+	data: JSON.stringify({
+    task: text
+  }),
+  dataType:"JSON"
+});
     //get user input
-    $('.ToDo').append('<li>' + text + ' <button class="delete">Delete</button></li>');
-    $('.text').val('');
+    // $('.ToDo').append('<li>' + text + ' <button class="delete">Delete</button></li>');
+    // $('.text').val('');
 }
 
+function renderTodo(){
+  var settings = {
+    url: 'https://tiny-za-server.herokuapp.com/collections/Chris_list',
+    type: 'GET',
+    success: function(list, status, xhr) {
+    var listItems =  list.map(function(task,i,arr){
+return $('<li>'+task.task+'<button class="delete">Delete</button></li>');
+});
+$('.ToDo').empty();
+$('.ToDo').append(listItems);
+
+
+        // navigate to post list
+console.log(list);
+
+    },
+    error: function() {
+        // log an error
+        console.log('the item didn\'t work');
+    },
+
+
+};
+// console.log(post);
+$.ajax(settings);
+
+
+}
+renderTodo();
 function deleteListItem() {
     $(this).parent().remove();
 }
@@ -24,37 +70,3 @@ $(function() {
 //when data recieved display data
 // });
 // function getList (){
-var settings = {
-    url: 'http://tiny-za-server.herokuapp.com/collections/Chris_list',
-    type: 'GET',
-    success: function(list, status, xhr) {
-        // navigate to post list
-
-    },
-    error: function() {
-        // log an error
-        console.log('the item didn\'t work');
-    },
-
-
-};
-// console.log(post);
-$.ajax(settings);
-
-
-// var settings = {
-// 	url: 'http://tiny-za-server.herokuapp.com/collections/Chris_list',
-// 	type: 'POST',
-// 	success: function(todo, status, xhr) {
-// 		// navigate to post list
-
-// 	},
-// 	error: function() {
-// 		// log an error
-// 		console.log('the item didn\'t work');
-// 	},
-// 	contentType: 'application/json',
-// 	// data: JSON.stringify(post)
-// };
-// // console.log(post);
-// $.ajax(settings);
